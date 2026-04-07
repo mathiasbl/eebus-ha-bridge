d6891d98a2255e8bf265a8e8ab341e9289bebecb
volschin <volschin@googlemail.com>
2026-04-07 00:44:35 +0000

    feat: add LPC and monitoring use case wrappers with event routing
    Implements LPCWrapper (eg/lpc) and MonitoringWrapper (ma/mpc) that adapt
    the eebus-go EntityEventCallback to the internal EventBus. Includes 8
    tests covering all event types and unknown-event suppression.

---
+++ eebus-bridge/internal/usecases/monitoring_test.go
@@ -0,0 +1,81 @@
+package usecases_test
+
+import (
+	"testing"
+	"time"
+
+	mampc "github.com/enbility/eebus-go/usecases/ma/mpc"
+	"github.com/volschin/eebus-bridge/internal/eebus"
+	"github.com/volschin/eebus-bridge/internal/usecases"
+)
+
+func TestMonitoringEventRouting(t *testing.T) {
+	bus := eebus.NewEventBus()
+	ch := bus.Subscribe()
+	defer bus.Unsubscribe(ch)
+
+	monWrapper := usecases.NewMonitoringWrapper(bus)
+
+	monWrapper.HandleEvent("test-ski", nil, nil, mampc.DataUpdatePower)
+
+	select {
+	case evt := <-ch:
+		if evt.Type != "monitoring.power_updated" {
+			t.Errorf("Type = %q, want monitoring.power_updated", evt.Type)
+		}
+	case <-time.After(time.Second):
+		t.Fatal("timeout waiting for monitoring event")
+	}
+}
+
+func TestMonitoringEnergyConsumedEventRouting(t *testing.T) {
+	bus := eebus.NewEventBus()
+	ch := bus.Subscribe()
+	defer bus.Unsubscribe(ch)
+
+	monWrapper := usecases.NewMonitoringWrapper(bus)
+	monWrapper.HandleEvent("ski-2", nil, nil, mampc.DataUpdateEnergyConsumed)
+
+	select {
+	case evt := <-ch:
+		if evt.Type != "monitoring.energy_consumed_updated" {
+			t.Errorf("Type = %q, want monitoring.energy_consumed_updated", evt.Type)
+		}
+	case <-time.After(time.Second):
+		t.Fatal("timeout waiting for monitoring energy consumed event")
+	}
+}
+
+func TestMonitoringFrequencyEventRouting(t *testing.T) {
+	bus := eebus.NewEventBus()
+	ch := bus.Subscribe()
+	defer bus.Unsubscribe(ch)
+
+	monWrapper := usecases.NewMonitoringWrapper(bus)
+	monWrapper.HandleEvent("ski-3", nil, nil, mampc.DataUpdateFrequency)
+
+	select {
+	case evt := <-ch:
+		if evt.Type != "monitoring.frequency_updated" {
+			t.Errorf("Type = %q, want monitoring.frequency_updated", evt.Type)
+		}
+	case <-time.After(time.Second):
+		t.Fatal("timeout waiting for monitoring frequency event")
+	}
+}
+
+func TestMonitoringUnknownEventIgnored(t *testing.T) {
+	bus := eebus.NewEventBus()
+	ch := bus.Subscribe()
+	defer bus.Unsubscribe(ch)
+
+	monWrapper := usecases.NewMonitoringWrapper(bus)
+	monWrapper.HandleEvent("ski-x", nil, nil, "unknown-event-type")
+
+	select {
+	case evt := <-ch:
+		t.Errorf("unexpected event published: %+v", evt)
+	case <-time.After(50 * time.Millisecond):
+		// expected: no event published
+	}
+}
